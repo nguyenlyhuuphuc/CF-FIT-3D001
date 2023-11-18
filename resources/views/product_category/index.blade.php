d5362ce2caa5dbf7827ffd1d2f9b6fc0322e929d
phuc <ngoctannew@gmail.com>
2023-11-18 21:37:31 +0700

    18/11/2023

---
+++ resources/views/product_category/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.master')
+
+@section('content')
+    <h1>Product Category List Blade Template Extends Parent</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blade.view-blade');
+});
+
+Route::get('master', function (){
+    return view('layout.master');
+});
+
+Route::get('product/index', function (){
+    return view('product.index');
+});
+
+Route::get('product_category/index', function (){
+    return view('product_category.index');
+});
+
+
+
+Route::get('product/create', function (){
+    echo 'Tao san pham';
+});
+
+Route::get('product/detail/{id}/{productId?}', function ($id, $productId = null){
+    echo "Detail san pham id : $id danh muc san pham : $productId";
 });
