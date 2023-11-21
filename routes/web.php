18f0fb0d6d9fa74147eb14d620fe7545b996f744
phuc <ngoctannew@gmail.com>
2023-11-21 21:34:42 +0700

    21/11/2023

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('admin', function (){
+    return view('admin.layout.master');
+});
 
 Route::get('/', function (){
     return view('client.layout.master');
 });
+
+Route::get('home',function(){
+    return view('client.pages.home');
+});
+
+Route::get('shop-grid',function(){
+    return view('client.pages.shop-grid');
+});
+
+Route::get('shop-detail',function(){
+    return view('client.pages.shop-detail');
+});
+
